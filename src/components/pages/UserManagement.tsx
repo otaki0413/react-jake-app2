00b4075876cb65501a6c1ec4fe72a28406075749
otaki0413 <takida0413@gmail.com>
2023-03-21 03:29:10 +0900

    ✨ ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,37 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
+  // 全てのユーザーを取得する用のカスタムフックを実行
+  const { getUsers, loading, users } = useAllUsers();
+
+  // 初回マウント時に getUsers を実行する
+  useEffect(() => getUsers, []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="おたき"
-          fullName="otaki"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap
+          p={{ base: 4, md: 10 }}
+          justify="center"
+        >
+          {users?.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,31 @@
+import { useMessage } from "./useMessage";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { User } from "../types/api/user";
+
+// 全てのユーザーを取得する用のカスタムフック
+export const useAllUsers = () => {
+  // ユーザー取得時のメッセージ用のカスタムフックを実行
+  const { showMessage } = useMessage();
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState<User[]>();
+
+  // 全てのユーザーを取得する関数
+  const getUsers = useCallback(() => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch(() => {
+        showMessage({
+          title: "ユーザー取得に失敗しました。",
+          status: "error",
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  // 返り値
+  return { getUsers, loading, users };
+};
